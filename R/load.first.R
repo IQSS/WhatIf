31bc15b5d8326153e6801f65f9c813f59f119a27
tercer <jhonaker@iq.harvard.edu>
2014-01-07 12:50:45 -0500

    fix of commands for .onAttach descriptions

--- R/load.first.R
+++ R/load.first.R
@@ -1,10 +1,14 @@
 .onAttach <- function(...) {
 
     mylib <- dirname(system.file(package = "WhatIf"))
-    ver <- packageDescription("WhatIf", lib = mylib)$Version
-    builddate <- packageDescription("WhatIf", lib = mylib)$Date
-    cat("#######################################################\n")
-    cat(paste("## \n##  WhatIf (Version ", ver, ", built ", builddate, ")\n", sep = "")) 
-    cat("##  Complete documentation available from http://gking.harvard.edu/whatif \n")
-    cat("## \n#######################################################\n")
+    ver <- packageDescription("WhatIf", lib.loc = mylib)$Version
+    builddate <- packageDescription("WhatIf", lib.loc = mylib)$Date
+
+    # WhatIf Info - do not exceed 80char/line
+ packageStartupMessage(paste("#######################################################\n",
+"##\n",
+"##  WhatIf (Version ", ver, ", built ", builddate, ")\n",
+"##  Complete documentation available from http://gking.harvard.edu/whatif \n",
+"##\n",
+"#######################################################", sep=""))
 }
