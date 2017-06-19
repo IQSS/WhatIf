419038dd1e086257ff25671f45d6dd0a46317d0c
christophergandrud <christopher.gandrud@gmail.com>
2017-06-19 15:40:48 -0400

    :book: remove onAttach message, conflicts with devtools, new docs

--- R/load.first.R
+++ R/load.first.R
@@ -1,14 +1,17 @@
-.onAttach <- function(...) {
+# Conflicts with devtools::document() so commented out
 
-    mylib <- dirname(system.file(package = "WhatIf"))
-    ver <- packageDescription("WhatIf", lib.loc = mylib)$Version
-    builddate <- packageDescription("WhatIf", lib.loc = mylib)$Date
+#.onAttach <- function(...) {
+
+#    mylib <- dirname(system.file(package = "WhatIf"))
+#    ver <- packageDescription("WhatIf", lib.loc = mylib)$Version
+#    builddate <- packageDescription("WhatIf", lib.loc = mylib)$Date
 
     # WhatIf Info - do not exceed 80char/line
- packageStartupMessage(paste("#######################################################\n",
-"##\n",
-"##  WhatIf (Version ", ver, ", built ", builddate, ")\n",
-"##  Complete documentation available from http://gking.harvard.edu/whatif \n",
-"##\n",
-"#######################################################", sep=""))
-}
+# packageStartupMessage(paste("#######################################################\n",
+#"##\n",
+##"##  WhatIf (Version ", ver, ", built ", builddate, ")\n",
+#"##  WhatIf\n",
+#"##  Complete documentation available from http://gking.harvard.edu/whatif \n",
+#"##\n",
+#"#######################################################", sep=""))
+#}
