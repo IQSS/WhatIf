f6ae3960fbb3c04bc39df6d94fff3676e1fe6f87
mbsabath <bsabath1@gmail.com>
2017-06-19 15:50:39 -0400

    Fixing loading issues

--- R/load.first.R
+++ R/load.first.R
@@ -1,20 +1,5 @@
-<<<<<<< HEAD
+
 #' @importFrom utils packageDescription
-.onAttach <- function(...) {
-
-    mylib <- dirname(system.file(package = "WhatIf"))
-    ver <- packageDescription("WhatIf", lib.loc = mylib)$Version
-    builddate <- packageDescription("WhatIf", lib.loc = mylib)$Date
-
-    # WhatIf Info - do not exceed 80char/line
- packageStartupMessage(paste("#######################################################\n",
-"##\n",
-"##  WhatIf (Version ", ver, ", built ", builddate, ")\n",
-"##  Complete documentation available from http://gking.harvard.edu/whatif \n",
-"##\n",
-"#######################################################", sep=""))
-}
-=======
 # Conflicts with devtools::document() so commented out
 
 #.onAttach <- function(...) {
@@ -32,4 +17,4 @@
 #"##\n",
 #"#######################################################", sep=""))
 #}
->>>>>>> master
+
